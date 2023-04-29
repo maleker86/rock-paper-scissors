85db4ab8b88a654117156fd8181e90825a384ef6
Kris <109919530+maleker86@users.noreply.github.com>
2023-04-29 16:47:31 -0700

    Add indentations to console & code for readability.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //script.js
 
-//NOTE TO SELF: THE COMPUTER CHOICE NEEDS TO NOT BE PRINTED…BEFORE THE PLAYER CHOOSES A THING :)
-
 //icky global variables bc baby's first program
 let roundCount = 0;
 let computerCount = 0;
@@ -9,66 +7,86 @@ let playerCount = 0;
 
 //get & set computerSelection
 const choices = [
-"rock",
-"paper",
-"scissors"
+    "rock",
+    "paper",
+    "scissors"
 ];
 
 function getComputerChoice() {
 const random = Math.floor(Math.random() * choices.length);
 let computerChoice = (random, choices[random]);
-console.log(`Computer wrote ${computerChoice}`);
-return computerChoice;
+    console.log(`     Computer wrote ${computerChoice}`);
+    return computerChoice;
 
 }
 
 //get & set case-insensitive playerSelection
 function getPlayerChoice() {
 let playerChoice = prompt("Rock, Paper, or Scissors?", "").toLowerCase();
-console.log(`Player wrote ${playerChoice}`);
-switch (playerChoice) { 
-    case "rock":
 
-// console.log("rock");
-break;
-case "paper":
-// console.log("paper");
-break;
-case "scissors":
-// console.log("scissors");
-break;
-default:
-console.log("please type rock paper or scissors");
-}return playerChoice;
+    console.log(`     Player wrote ${playerChoice}`);
+
+    switch (playerChoice) { 
+    case "rock":
+        break;
+    case "paper":
+        break;
+    case "scissors":
+        break;
+    default:
+        console.log("     Please type rock paper or scissors");
+    }
+    
+    return playerChoice;
 
 }
 
 //round func compares player & computer choices
-function playRound(playerChoice,computerChoice) {switch (true) { case (computerChoice === playerChoice) : console.log("It's a tie"); break; case (computerChoice === "paper" && playerChoice === "rock") : console.log(`You lose! ${computerChoice} beats ${playerChoice}.`); computerCount = ++computerCount; break; case (computerChoice === "scissors" && playerChoice === "paper") : console.log(`You lose! ${computerChoice} beats ${playerChoice}.`); computerCount = ++computerCount; break; case (computerChoice === "rock" && playerChoice === "scissors") : console.log(`You lose! ${computerChoice} beats ${playerChoice}.`); computerCount = ++computerCount; break; case (playerChoice === "paper" && computerChoice === "rock") : console.log(`You win! ${playerChoice} beats ${computerChoice}.`); playerCount = ++playerCount; break; case (playerChoice === "scissors" && computerChoice === "paper") : console.log(`You win! ${playerChoice} beats ${computerChoice}.`); playerCount = ++playerCount; break; case (playerChoice === "rock" && computerChoice === "scissors") : console.log(`You win! ${playerChoice} beats ${computerChoice}.`); playerCount = ++playerCount; break; default: console.log("The game broke. Please try again."); }
-
+function playRound(playerChoice,computerChoice) {
+    switch (true) { 
+        case (computerChoice === playerChoice) : 
+            console.log("     It's a tie"); 
+            break; 
+        case (computerChoice === "paper" && playerChoice === "rock") : 
+            console.log(`     You lose! ${computerChoice} beats ${playerChoice}.`); computerCount = ++computerCount; 
+            break; 
+        case (computerChoice === "scissors" && playerChoice === "paper") : 
+            console.log(`     You lose! ${computerChoice} beats ${playerChoice}.`); computerCount = ++computerCount; 
+            break; 
+        case (computerChoice === "rock" && playerChoice === "scissors") : 
+            console.log(`     You lose! ${computerChoice} beats ${playerChoice}.`); computerCount = ++computerCount; 
+            break; 
+        case (playerChoice === "paper" && computerChoice === "rock") : 
+            console.log(`     You win! ${playerChoice} beats ${computerChoice}.`); playerCount = ++playerCount; 
+            break; 
+        case (playerChoice === "scissors" && computerChoice === "paper") : 
+            console.log(`     You win! ${playerChoice} beats ${computerChoice}.`); playerCount = ++playerCount; 
+            break; 
+        case (playerChoice === "rock" && computerChoice === "scissors") : 
+            console.log(`     You win! ${playerChoice} beats ${computerChoice}.`); playerCount = ++playerCount; 
+            break; 
+        default: 
+            console.log("     A winner could not be determined."); }
 }
 
 //calls new Game
 function Game() {
-
 //new round
-for (i = 0; i <= 4; i++) {
-roundCount = ++roundCount;
-console.log(`It is Round ${roundCount} of 5.`);
-playRound(getPlayerChoice(),getComputerChoice());
-}
-
+    for (i = 0; i <= 4; i++) {
+        roundCount = ++roundCount;
+        console.log(`It is Round ${roundCount} of 5.`);
+        playRound(getPlayerChoice(),getComputerChoice());
 //print round winner count
-console.log(`Computer has won ${computerCount} and player has won ${playerCount}`);
-
+        console.log(`     Computer has won ${computerCount} and player has won ${playerCount}.`);
+    }
 //determine overall winner
-if (playerCount > computerCount) {
-console.log("Overall, Player wins");
-} else if (playerCount < computerCount) {
-console.log("Overall, Comp wins");
-} else {
-console.log("Overall, It's a Tie");
-}
+    if (playerCount > computerCount) {
+        console.log("It is Game Over. Player wins!");
+    } else if (playerCount < computerCount) {
+        console.log("It is Game Over. Computer wins.");
+    } else {
+        console.log("It is Game Over. It's a Tie!");
+    }
 }
 
 Game();
