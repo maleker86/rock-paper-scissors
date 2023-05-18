5237cc190172bd260267652f24d612c1464f2570
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-17 18:41:30 -0700

    Re-introduces Winner/Loser readout; BUG: no round null

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let roundCount = 0;
 let computerCount = 0;
 let playerCount = 0;
 
-let playerChoice;
-let computerChoice;
+let playerChoice = null;
+let computerChoice = null;
 
 //get & set computerSelection
 const choices = [
@@ -49,23 +49,35 @@ scissors.addEventListener('click', function (e) {
 
 function getPlayerChoice() {
     if (playerChoice) {
+        // console.log("P Result got");
         console.log(`Player wrote ${playerChoice}`);
-        playerChoice = null;
-    } else {
-        console.log("Please make a choice");
-    }
-};
-
-function playRound() {
-    if (playerChoice) {
-        console.log(`Player wrote ${playerChoice}`);
-        getComputerChoice();
-    playerChoice = null;
+        return playerChoice;
     } else {
         console.log("Please make a selection");
     }
 };
 
+function playRound(playerChoice,computerChoice) {
+    switch (true) { 
+        case (playerChoice === computerChoice) : 
+        console.log("tie"); break; 
+        case (playerChoice === "paper" && computerChoice === "rock") : 
+        console.log("player win"); break; 
+        case (playerChoice === "paper" && computerChoice === "scissors") : 
+        console.log("player lose"); break; 
+        case (playerChoice === "scissors" && computerChoice === "rock") : 
+        console.log("player lose"); break; 
+        case (playerChoice === "scissors" && computerChoice === "paper") : 
+        console.log("player lose"); break; 
+        case (playerChoice === "rock" && computerChoice === "paper") : 
+        console.log("player lose"); break; 
+        case (playerChoice === "rock" && computerChoice === "scissors") : 
+        console.log("player win"); break; 
+        default: console.log("woah"); break; 
+    } 
+        playerChoice = null;
+}
+
 done.addEventListener('click', function (e) {
-    playRound();
+    playRound(getPlayerChoice(),getComputerChoice());
 });
